e77bf9ebca400e319934431d75d6317c22246192
Gabriel Souza <souzagabrielluis@gmail.com>
2018-08-30 23:09:57 -0300

    basic monitoring services

---
+++ monitor/HardDrive.py
@@ -0,0 +1,17 @@
+class HardDrive:
+    Partition = ""
+    PercentUsed  = 0
+    Used = 0
+    Free = 0
+    Total = 0
+    Slave = False
+
+    def __init__(self): 
+        self.Partition = ""
+
+    def __init__(self, partition, percentUsed, total, used, free):
+        self.Partition = partition
+        self.PercentUsed = percentUsed
+        self.Total = total
+        self.Used = used
+        self.Free = free
--- monitor/routes.py
+++ monitor/routes.py
@@ -1,10 +1,40 @@
 from flask import Blueprint, render_template, flash, redirect, url_for, request
-import psutil,json
+from iismonitorpy.monitor.HardDrive import HardDrive
+import psutil, json
+
 monitor_api = Blueprint('monitor_api', __name__, template_folder='templates')
 
 
 
 @monitor_api.route("/cpu")
 def getCPU():
-    
     return json.dumps(psutil.cpu_percent())
+
+@monitor_api.route("/memory")
+def getMemory():
+    return json.dumps(psutil.virtual_memory().percent)
+
+
+@monitor_api.route("/diskpartitions")
+def getDisk():
+    dps = psutil.disk_partitions()
+    fmt_str = "{:<8} {:<7} {:<7}"
+    print(psutil.disk_usage('C:\\'))
+
+    hardDrives = []
+    # hardDrives.append(fmt_str.format("Drive", "Type", "Opts"))
+    for dp in dps:
+        if dp.opts != 'cdrom' : 
+            diskUsage = psutil.disk_usage(dp.device)
+            # ((986498199552 /1024) / 1024 ) / 1024 
+            #                           KB          MB      GB          
+            totalGB = (((diskUsage.total / 1024) / 1024) / 1024)
+            usedGb = (((diskUsage.used / 1024) / 1024) / 1024)
+            freeGB  = totalGB - usedGb
+            hd = HardDrive(dp.device, diskUsage.percent, totalGB, usedGb, freeGB)
+
+           # hardDrives.append(fmt_str.format(dp.device, dp.fstype, dp.opts))
+            hardDrives.append(hd)
+    #return json.dumps(dps[0])
+    return json.dumps([p.__dict__ for p in hardDrives]) 
+
